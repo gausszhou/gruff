ae4170c6b198130eecce22380141ea06c81b7976
gausszhou <gausszhou@qq.com>
2026-05-23 21:20:44 +0800

    refactor: deduplicate testdata, benchmark reads from root testdata

--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -1,24 +1,34 @@
 package benchmark
 
 import (
-	_ "embed"
+	"os"
 	"testing"
 
 	"github.com/charmbracelet/glamour"
 	"github.com/gausszhou/gruff"
 )
 
-//go:embed testdata/sample.md
-var sampleMD string
-
 func BenchmarkGruff(b *testing.B) {
+	source, err := os.ReadFile("../testdata/sample.md")
+	if err != nil {
+		b.Fatal(err)
+	}
+	input := string(source)
+
 	b.ReportAllocs()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		gruff.Render(sampleMD)
+		gruff.Render(input)
 	}
 }
 
 func BenchmarkGlamour(b *testing.B) {
+	source, err := os.ReadFile("../testdata/sample.md")
+	if err != nil {
+		b.Fatal(err)
+	}
+	input := string(source)
+
 	r, err := glamour.NewTermRenderer(
 		glamour.WithStandardStyle("dark"),
 	)
@@ -27,7 +37,8 @@ func BenchmarkGlamour(b *testing.B) {
 	}
 
 	b.ReportAllocs()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		r.Render(sampleMD)
+		r.Render(input)
 	}
 }
